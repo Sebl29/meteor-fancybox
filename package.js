92a7625fddd048d7f1e2b36a38ea9654e5f84d0b
Sebl29 <portal@datenwei.de>
2014-12-03 13:05:50 +0100

    forked for adding jquery.mousewheel and the fancybox.helpers.

--- package.js
+++ package.js
@@ -1,18 +1,28 @@
 Package.describe({
-    summary : "Responsive media / lightbox repackaged for meteor"
+    name : 'fancybox-full',
+    summary : "Responsive media / lightbox repackaged for meteor; full package, including jquery.mousewheel-3 and fancybox.helpers. License: http://creativecommons.org/licenses/by-nc/3.0/ (plz see http://www.fancyapps.com/fancybox/#license); forked from https://github.com/matteodem/meteor-fancybox/ (by https://github.com/matteodem) and https://github.com/fancyapps/fancyBox (by Janis Skarnelis - janis@fancyapps.com)",
+    version: '2.1.5',
+    git: 'https://github.com/Sebl29/meteor-fancybox'
 });
 
 Package.on_use(function(api) {
     api.use('jquery', 'client');
     api.add_files([
-        'lib/jquery.fancybox.css',
-        'lib/jquery.fancybox.pack.js',
-        'lib/fancybox_loading.gif',
-        'lib/fancybox_loading@2x.gif',
-        'lib/fancybox_overlay.png',
-        'lib/fancybox_sprite.png',
-        'lib/fancybox_sprite@2x.png',
-        'lib/blank.gif',
-		'path-override.css']
-        , 'client');
+        'lib/1-lib/jquery.mousewheel-3.0.6.pack.js',
+        'lib/core/jquery.fancybox.css',
+        'lib/core/jquery.fancybox.pack.js',
+        'lib/core/fancybox_loading.gif',
+        'lib/core/fancybox_loading@2x.gif',
+        'lib/core/fancybox_overlay.png',
+        'lib/core/fancybox_sprite.png',
+        'lib/core/fancybox_sprite@2x.png',
+        'lib/core/blank.gif',
+        'lib/helpers/fancybox_buttons.png',
+        'lib/helpers/jquery.fancybox-buttons.css',
+        'lib/helpers/jquery.fancybox-buttons.js',
+        'lib/helpers/jquery.fancybox-media.js',
+        'lib/helpers/jquery.fancybox-thumbs.css',
+        'lib/helpers/jquery.fancybox-thumbs.js',
+		'path-override.css'],
+        'client');
 });
